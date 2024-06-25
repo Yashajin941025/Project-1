880c264cf4aeed155dc9b10582dc48e4f8095c0f
Alice <138005616+YashajinAlice@users.noreply.github.com>
2024-06-25 21:43:45 +0800

    logs頻道監聽系統創建

--- set/settings.js
+++ set/settings.js
@@ -1,15 +1,17 @@
 module.exports = {
-    "logChannelName": "logs-text",
+    "logChannelName": "logs-test",
     "warnFilePath": "./warn.json",
     "muteDurations": {
         "3": 86400000,
         "5": 604800000
     },
     "banAfterWarnings": 6,
-    "countingChannelId": "1191949142772154439",
+    "countingChannelId": null,
     "currentNumber": 0,
     "startingNumber": 0,
     "lastUserId": null,
-    "logChannelId": null, // 日誌頻道ID
-    "logEvents": [] // 需要監聽的事件
+    "logChannelId": "1254453106159124631",
+    "logEvents": [
+        "messageUpdate"
+    ]
 };
